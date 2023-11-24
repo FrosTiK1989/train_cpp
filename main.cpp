3e4108a228655d78d25a96e7c064f597fc4ccddc
Денис Маресов <frostik1989@yandex.ru>
2023-11-24 16:48:25 +0300

    convert metors in kilommetors

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
 
 int main() {
-    int a = {8};
-    int b = {++a};
-    int c = {b++};
-    cout << "a: " << a << "\t b: " << b << endl;
-    cout << "c: " << c << "\t a + c: " << a + c << endl;
+    // Создадим переменную для ввода от пользователя
+    int metors {0};
+    //Создадим константу на 1 км
+    constexpr int metorsInKm {1000};
+    // Запросим сколько метров есть
+    std::cout << "Сколько метров надо перевести?: ";
+    std::cin >> metors;
+    std::cout << "Вычисляю..." << std::endl;
+    // Запишем результат в переменные
+    const int km = metors / metorsInKm;
+    const int metor = metors % metorsInKm;
+    // Выведем результат в консоль
+    std::cout << "У меня есть результат" << std::endl;
+    std::cout << km << " километра и " << metor << "метров.";
     return 0;
 }
