cf090baee9e5a267b4bc5d02cca5fa64e318836f
Денис Маресов <frostik1989@yandex.ru>
2023-11-30 00:09:49 +0300

    add new learn and function on main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 
-int main() {
+using namespace std;
+
+int learn() {
+    // Создадим константу pi и присвоим ей значение 3.14
+    const double pi = {3.14};
+    /* Создадим переменную типа double для хранения данных от
+    пользователя */
+    double radius = {0};
+    /* Выведем приветственное сообщение пользователю и запросим
+     у него радус окружности */
+    cout << "Привет, мой друг!" << endl;
+    cout << "Какой радиус вычисляемой окружности? ";
+    // Получим данные от поьзователя и запишем в переменну radius
+    cin >> radius;
+    // Сделаем вид, что работаем
+    cout << "Начинаю вычислене...подождите" << endl;
+    // Производим вычисления и запишем их в переменную square
+    const double square = pi * pow(radius, 2);
+    // Озвучиваем результат пользователю
+    cout << "Площадь вашей окружнности равна: " << square << endl;
+    return 0;
+}
+
+int metors() {
     // Создадим переменную для ввода от пользователя
-    int metors {0};
+    int metors = {0};
     //Создадим константу на 1 км
-    constexpr int metorsInKm {1000};
+    const int metorsInKm = {1000};
     // Запросим сколько метров есть
-    std::cout << "Сколько метров надо перевести?: ";
-    std::cin >> metors;
-    std::cout << "Вычисляю..." << std::endl;
+    cout << "Сколько метров надо перевести?: ";
+    cin >> metors;
+    cout << "Вычисляю..." << endl;
     // Запишем результат в переменные
     const int km = metors / metorsInKm;
     const int metor = metors % metorsInKm;
     // Выведем результат в консоль
-    std::cout << "У меня есть результат" << std::endl;
-    std::cout << km << " километра и " << metor << "метров.";
+    cout << "У меня есть результат" << endl;
+    cout << km << " километра и " << metor << " метров." << endl;
     return 0;
 }
+
+int main() {
+    learn();
+    metors();
+    return 0;
+}
+
