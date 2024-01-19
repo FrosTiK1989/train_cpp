48c5d1f5eac1103c64787d5205a9610f695f3bbf
Денис Маресов <frostik1989@yandex.ru>
2024-01-19 16:51:01 +0300

    New case from learning

--- main.cpp
+++ main.cpp
@@ -1,72 +1,19 @@
 #include <iostream>
 
-using namespace std;
-
-int learn() {
-    // Создадим константу pi и присвоим ей значение 3.14
-    const double pi = {3.14};
-    /* Создадим переменную типа double для хранения данных от
-    пользователя */
-    double radius = {0};
-    /* Выведем приветственное сообщение пользователю и запросим
-     у него радус окружности */
-    cout << "Привет, мой друг!" << endl;
-    cout << "Какой радиус вычисляемой окружности? ";
-    // Получим данные от поьзователя и запишем в переменну radius
-    cin >> radius;
-    // Сделаем вид, что работаем
-    cout << "Начинаю вычислене...подождите" << endl;
-    // Производим вычисления и запишем их в переменную square
-    const double square = pi * pow(radius, 2);
-    // Озвучиваем результат пользователю
-    cout << "Площадь вашей окружнности равна: " << square << endl;
-    return 0;
-}
-
-int metors() {
-    // Создадим переменную для ввода от пользователя
-    int metors = {0};
-    //Создадим константу на 1 км
-    const int metorsInKm = {1000};
-    // Запросим сколько метров есть
-    cout << "Сколько метров надо перевести?: ";
-    cin >> metors;
-    cout << "Вычисляю..." << endl;
-    // Запишем результат в переменные
-    const int km = metors / metorsInKm;
-    const int metor = metors % metorsInKm;
-    // Выведем результат в консоль
-    cout << "У меня есть результат" << endl;
-    cout << km << " километра и " << metor << " метров." << endl;
-    return 0;
-}
-
-int converter() {
-    /*
-        Запросим текущий курс доллара к рублю
-        и создадим переменную curs для ее хранения
-     */
-    double curs = {0};
-    cout << "Какой нынче курс доллара к рублю?: ";
-    cin >> curs;
-    /*
-        Поблагодарим пользователя и спросим сколько рублей у него есть,
-        предварительно создадим переменную rubles
-     */
-    double rubles = {0};
-    cout << "Спасибо! \nА сколько рублей надо перевести в доллары?: ";
-    cin >> rubles;
-    // Создадим переменную для вычислления и назовем ее dollar
-    const double dollar = rubles / curs;
-    // Выдадим результат пользователю
-    cout << "Я тут посчитал и получается, что у нас выходит " << dollar << "$." << endl;
-    return 0;
-}
-
 int main() {
-    learn();
-    metors();
-    converter();
-    return 0;
+    // Инициализируем две переменные для сохранения информации от пользователя.
+    int firstDigit {};
+    int secondDigit {};
+    // Выводим в консоль запрос на ввод этих чисел.
+    std::cout << "Insert first digit: " << std::endl;
+    std::cin >> firstDigit;
+    std::cout << "Insert second digit: " << std::endl;
+    std::cin >> secondDigit;
+    // Благодарим пользователя и уходим на вычисление.
+    std::cout << "Thanks! \nNow I'll compare these numbers." << std::endl;
+    // Начинаем процесс сравнения введенных чисел.
+    if(firstDigit != secondDigit)
+        std::cout << "Digits not equals" << std::endl;
+    else
+        std::cout << "Digits is equals" << std::endl;
 }
-
